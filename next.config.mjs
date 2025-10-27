a9165282c1fb2440fb1238fa513f87d98aaf7a31
Grimmjow06100 <samy06.fr@gmail.com>
2025-10-27 18:20:34 +0100

    zzzz

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@
   const nextConfig = {
      images: {
         remotePatterns:[new URL('http://localhost:8080/mangas/**')],
-        unoptimized: true
+        unoptimized: true,
+
       }
 
   };
